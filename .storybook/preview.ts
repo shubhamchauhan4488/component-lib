b01ed3b416b4d1800cfabca45fa92b6a97ef633c
shubhamchauhan <shubhamchauhan4488@gmail.com>
2025-03-24 01:31:48 -0400

    feat: add a11y addon and autodocs for all components

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,14 @@
 import type { Preview } from '@storybook/react'
 
 const preview: Preview = {
+  tags: ['autodocs'],
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
-
     a11y: {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
